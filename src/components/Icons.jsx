41a86f9aa23de6709b5b196bd01e1e7aa9bc6ff5
manuel lantieri <manuel.lantieri02@gmail.com>
2025-02-01 11:55:25 +0100

    feat add icons

---
+++ src/components/Icons.jsx
@@ -0,0 +1,35 @@
+function Icons() {
+  return (
+    <section className="icons">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <img src="/imgs/buy-comics-shop-locator.png" alt="digital-comic" />
+            <a href="#">Digital Comics</a>
+          </div>
+          <div className="col">
+            <img src="/imgs/buy-comics-merchandise.png" alt="DC Merchandise" />
+            <a href="#">DC Merchandise</a>
+          </div>
+          <div className="col">
+            <img src="/imgs/buy-comics-subscriptions.png" alt="Subscription" />
+            <a href="#">Subscription</a>
+          </div>
+          <div className="col">
+            <img
+              src="/imgs/buy-comics-shop-locator.png"
+              alt="Comic Store Locator"
+            />
+            <a href="#">Comic Store Locator</a>
+          </div>
+          <div className="col">
+            <img src="/imgs/buy-dc-power-visa.svg" alt="DC Power VISA" />
+            <a href="#">DC Power VISA</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Icons;
