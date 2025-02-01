d434b14a5e7e560fd8a4742690596b4d2d049668
manuel lantieri <manuel.lantieri02@gmail.com>
2025-02-01 11:05:52 +0100

    feat add main

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import Icons from "./components/Icons";
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <Icons />
       <Footer />
     </>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,13 @@
+const content = "--> Content goes here <--";
+
 function Main() {
-  return <main></main>;
+  return (
+    <main>
+      <section className="container">
+        <h1> content </h1>
+      </section>
+    </main>
+  );
 }
 
 export default Main;
